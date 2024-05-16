da896b3f32ece64dc281bd45f971c7839632d1f4
nadja <nadja.kaelin@students.fhnw.ch>
2024-05-16 20:43:55 +0200

    initial experiments

--- app.py
+++ app.py
@@ -5,11 +5,29 @@ st.title("KI4RE - REMOTE WORKSHOPS")
 
 
 # Beispielbutton -----------------------------------------
-user_input = st.text_input("Geben Sie Ihren Text ein:")
-button_clicked = st.button("Absenden")
+tab1, tab2, tab3 = st.tabs(["Persönlich", "Kollaborativ", "Zusammengeführt"])
 
-if button_clicked:
-    # API-Aufruf hier einfügen
-    st.write("Verarbeitung...")
+with tab1:
+   st.header("Deine persönlichen Notizen")
 
-# Beispielbutton -----------------------------------------
+   if 'submissions' not in st.session_state:
+      st.session_state.submissions = []
+
+   if 'area_input' not in st.session_state:
+      st.session_state.area_input = ''
+
+   def submit():
+      st.session_state.submissions.append(st.session_state.input)
+      st.session_state.input = ''
+
+   for submission in st.session_state.submissions:
+      st.write(f'{submission}')
+
+   st.text_area('Notizen hinzufügen', key='input', on_change=submit)
+
+with tab2:
+   st.header("Eure gemeinsamen Notizen")
+
+with tab3:
+   st.header("Eure harmonisierten Notizen")
+
